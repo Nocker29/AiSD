66eadfbdc3899edce8101a5b2b755fd878a9e060
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-12-02 19:45:24 +0100

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,78 @@
+from typing import Any, Callable
+
+
+class BinaryNode:
+    value: Any
+    left_child: 'BinaryNode'
+    right_child: 'BinaryNode'
+
+    def __init__(self,value):
+        self.left_child = None
+        self.right_child = None
+        self.value = value
+
+    def is_leaf(self):
+        if self.left_child == None and self.right_child == None:
+            return True
+        return False
+
+    def add_left_child(self, value: Any):
+        self.left_child = BinaryNode(value)
+
+    def add_right_child(self, value: Any):
+        self.right_child = BinaryNode(value)
+
+    def traverse_in_order(self, visit: Callable[[Any], None]):
+        if self.left_child != None:
+            self.left_child.traverse_in_order(visit)
+        visit(self)
+        if self.right_child != None:
+            self.right_child.traverse_in_order(visit)
+
+    def traverse_post_order(self, visit: Callable[[Any], None]):
+        if self.left_child != None:
+            self.left_child.traverse_post_order(visit)
+        if self.right_child != None:
+            self.right_child.traverse_post_order(visit)
+        visit(self)
+
+    def traverse_pre_order(self, visit: Callable[[Any], None]):
+        visit(self)
+        if self.left_child != None:
+            self.left_child.traverse_pre_order()
+        if self.right_child != None:
+            self.right_child.traverse_pre_order()
+
+    def __str__(self):
+        return str(self.value)
+
+# node = BinaryNode(1)
+# node.add_left_child(2)
+# node.add_right_child(3)
+# node.left_child.add_left_child(4)
+# node.left_child.add_right_child(5)
+# node.right_child.add_left_child(6)
+# node.right_child.add_right_child(7)
+#
+# print(node)
+# print(f'{node.left_child} {node.right_child}')
+# print(f'{node.left_child.left_child} {node.left_child.right_child} '
+#       f'{node.right_child.left_child} {node.right_child.right_child}')
+
+class BinaryTree:
+    root: BinaryNode
+
+    def __init__(self, child:'BinaryNode'):
+        self.root = child
+
+    def traverse_in_orderd(self):
+        self.root.traverse_in_order()
+
+    def traverse_post_order(self, visit: Callable[[Any], None]):
+        self.root.traverse_post_order()
+
+    def traverse_pre_order(self, visit: Callable[[Any], None]):
+        self.root.traverse_pre_order()
+
+drzewo = BinaryTree(10)
+print(drzewo.root)
